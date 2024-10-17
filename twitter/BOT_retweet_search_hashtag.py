ae87033f5e365a879e7409b58efa1f96eb00fdd3
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-10-17 17:53:36 +0200

    Add files via upload

---
+++ twitter/BOT_retweet_search_hashtag.py
@@ -0,0 +1,50 @@
+import tweepy
+import time
+import datetime
+
+api_key="your api_key"
+api_key_secret="your api_key_secret"
+bearer="your bearer"
+access_token="your access_token"
+access_token_secret="your access_token_secret"
+
+autherticator= tweepy.OAuthHandler(api_key, api_key_secret)
+autherticator.set_access_token(access_token, access_token_secret)
+api = tweepy.API(autherticator)
+
+
+# Retweet a tweet that contains a hashtag
+
+def botRetweetHashtag(listHashtag):
+
+    #parametres d'attente
+    delaybetweenAction=1800
+    api = tweepy.API(autherticator)
+    lengthList=len(listHashtag)
+    index=0
+
+    while True:
+        print(f"\n{datetime.datetime.utcnow()}\n")
+
+        try:
+            hashtag=listHashtag[index]
+            search_query = "#" + hashtag + " -filter:retweets -filter:replies"
+            # -filter:retweets
+            tweets = tweepy.Cursor(api.search_tweets, q=search_query, lang="en").items(1)
+
+            for tweet in tweets:
+                print(tweet.text)
+                api.retweet(tweet.id) 
+                print('Comment\n\n')
+
+        except Exception as e1:
+            print('error', e1)
+
+        else:
+            a = 1
+
+        index = index + 1
+        q, index = divmod(index, lengthList) #index reset to 0 if index>lengthList
+        time.sleep(delaybetweenAction)
+
+botRetweetHashtag(["Bitcoin","BTC","ETH","Crypto","Web3","NFT"])
