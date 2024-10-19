06f344217fe653df9c8cd4bff6d60b32a9861bc2
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-10-19 17:18:02 +0200

    Add files via upload

---
+++ twitter/BOT_Bitcoin_price_dominance.py
@@ -0,0 +1,79 @@
+import tweepy
+import time
+from tradingview_ta import TA_Handler, Interval, Exchange
+
+api_key="your api_key"
+api_key_secret="your api_key_secret"
+bearer="your bearer"
+access_token="your access_token"
+access_token_secret="your access_token_secret"
+
+autherticator= tweepy.OAuthHandler(api_key, api_key_secret)
+autherticator.set_access_token(access_token, access_token_secret)
+api = tweepy.API(autherticator)
+
+#Get current price of a crypto
+def current_price(coin, exchange):
+    try:
+        tokenBTC = TA_Handler(
+            symbol=coin+"USDT",
+            screener="crypto",
+            exchange=exchange,
+            interval=Interval.INTERVAL_1_MINUTE
+        )
+        LastPriceBTC = tokenBTC.get_indicators()['open']
+        #print(LastPriceBTC)
+        return LastPriceBTC
+
+    except Exception as e1:
+        print('error', e1)
+    else:
+        a = 1
+    return 0
+#current_price("BTC","binance")
+
+#Save the fear and greed image
+def FearandGreed():
+
+    try:
+        rss_url = 'https://alternative.me/crypto/fear-and-greed-index.png'
+        res = requests.get(rss_url, stream=True)
+        if res.status_code == 200:
+            with open("FearGreed.png", 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded')
+        else:
+            print('Image Couldn\'t be retrieved')
+
+    except Exception as e1:
+        print('error', e1)
+    else:
+        a = 1
+
+#FearandGreed()
+
+
+#BOT that tweets every 30 minutes the bitcoin Dominance and BTC price
+def BitcoinDominance():
+
+    while True:
+
+        try:
+            lastPrice=current_price("BTC", "binance")
+            message = "#Bitcoin Fear and Greed index\n\n" + "Current #BTC price is " + str(round(float(lastPrice))) + " $"
+            message = message + "\n\n#Crypto #FearAndGreed\n\n"
+            image = "yourPath/FearGreed.png"
+            _media = api.media_upload(image)
+            api.update_status(status=message, media_ids=[_media.media_id])
+
+        except Exception as e1:
+            print('error', e1)
+            # return e1
+            print("Already tweeted or error")
+        else:
+             a=1
+
+        time.sleep(1800)
+
+BitcoinDominance()
+
---
+++ twitter/BOT_post_Images.py
@@ -0,0 +1,72 @@
+import tweepy
+import time
+import datetime
+from openpyxl import load_workbook
+
+api_key="your api_key"
+api_key_secret="your api_key_secret"
+bearer="your bearer"
+access_token="your access_token"
+access_token_secret="your access_token_secret"
+
+autherticator= tweepy.OAuthHandler(api_key, api_key_secret)
+autherticator.set_access_token(access_token, access_token_secret)
+api = tweepy.API(autherticator)
+
+#BOT that create a post with an image and a text every 30 minutes, from a list of .png and text saved in an Excel file
+def tweeter():
+
+    delaybetweenAction=1800
+    pathExcel = "yourExcelPath.xlsx" #Excel that contains the text of the comments
+
+    #Load the sheet named "Data" of the xlsx
+    wb = load_workbook(pathExcel)
+    index = 0
+    for index in range(len(wb.sheetnames)):
+        if wb.sheetnames[index] == "Data":
+            break
+    wb.active = index
+    sheet = wb.active
+
+    ListImage = []
+    ListMessage =[]
+
+    #Load the list of image (ex image1.png) and text in the Excel file, column A and B, start line 2.
+    j = 2
+    while str(sheet['A' + str(j)].value) != "None":
+        ListImage.append(sheet['A' + str(j)].value)
+        ListMessage.append(sheet['B' + str(j)].value)
+        j = j + 1
+    print(ListImage)
+    print(ListMessage)
+
+    NbImage = len(ListImage)
+
+    for indexImage in range(0, NbImage):
+
+        print(f"\n{datetime.datetime.utcnow()}\n")
+        print("index Message : " + str(indexImage))
+
+        try:
+            image = ListImage[indexImage]
+            message = ListMessage[indexImage]
+
+            _media = api.media_upload(image)
+            print(str(image),message)
+            api.update_status(status=message, media_ids=[_media.media_id])
+            print("Tweet created")
+
+        except Exception as e1:
+            print('error', e1)
+            # return e1
+            print("Already tweeted or error")
+        else:
+            a=1
+
+        time.sleep(delaybetweenAction)
+
+tweeter()
+
+
+
+
