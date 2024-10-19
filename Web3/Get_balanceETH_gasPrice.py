a99b108edca34fba449ede4f91b5283b7c78e294
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-10-19 17:30:41 +0200

    Add files via upload

---
+++ Web3/Get_balanceETH_gasPrice.py
@@ -0,0 +1,24 @@
+import random
+from web3 import Web3
+
+#import your API request, ex "https://eth-mainnet.g.alchemy.com/v2/" or infura
+web3=Web3(Web3.HTTPProvider('yourAPI'))
+
+def getbalanceETH(wallet_):
+    balance_wei = web3.eth.get_balance(wallet_)
+
+    # Convertir la balance de wei à ether
+    balance_eth = web3.fromWei(balance_wei, 'ether')
+
+    print(f'Balance : {round(balance_eth,5)} ETH')
+    return balance_eth
+#getbalanceETH(walletETH)
+
+def getCurrentGasPrice():
+
+    gas = web3.eth.gas_price
+    print("gas_", gas_)
+    return gas
+
+#getCurrentGasPrice()
+
